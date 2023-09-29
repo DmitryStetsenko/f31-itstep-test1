ac7d582b153aae0bdd6b8657c638614d5bb876c4
Oleksii Tuhai <alextugai73@gmail.com>
2023-09-29 13:59:30 +0200

    render brad-crumbs__items in js

--- js/script.js
+++ js/script.js
@@ -9,10 +9,26 @@ const btnCalcSum = doc.querySelector('.calculate-sum__btn');
 
 console.log('go');
 
+const bradCrumbsTitles = ['bradCrumpTitle 1','bradCrumpTitle 2','bradCrumpTitle 3','bradCrumpTitle 4'];
+renderBradCrumbsItems(bradCrumbsTitles);
+
+
+
 btnHelloWorld.onclick = () => console.log('Hello World!!');
+btnCalcSum.onclick = () => calcSumResult.innerText = calculateSum(calcSumInput1.value, calcSumInput2.value);
 
-btnCalcSum.onclick = () => calcSumResult.innerText = calculateSum(calcSumInput1.value, calcSumInput2.value)
 
+
+// ------------------functions------------------------
+
+function renderBradCrumbsItems(titles) {
+    titles.forEach(title => renderBradCrumbsItem('.brad-crumbs', title));
+}
+
+function renderBradCrumbsItem(paremtElementSelector, title) {
+    const parentElement = doc.querySelector(paremtElementSelector);
+    parentElement.innerHTML += `<li class="brad-crumbs__item"><a href="#">${title}</a></li>`;
+}
 
 function calculateSum(number1, number2) {
     return Number(number1) + Number(number2) || 0;
