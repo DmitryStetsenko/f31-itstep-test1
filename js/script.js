fda0b3d6fb5ff0cd1724c0f5428f49ac1eb275aa
Stimarc <rikudo7474@gmail.net>
2023-10-03 20:03:47 +0300

    todo js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 const doc = document;
 
-console.log(go);
+
+function addTodo() {
+    const newTodoTitle = doc.getElementById('newTodoTitle').value;
+
+    if (newTodoTitle.trim() !== '') {
+      
+      const newTodoItem = doc.createElement('li');
+      newTodoItem.className = 'todo';
+      newTodoItem.innerHTML = `
+        <div class="todo__content">
+          <div><span class="todo__number">1</span>.</div>
+          <input type="checkbox">
+          <div class="todo__title">${newTodoTitle}</div>
+        </div>
+        <button class="btn-delete">DEL</button>
+      `;
+      
+      const todoList = doc.getElementById('todo-list');
+      todoList.appendChild(newTodoItem);
+  
+      
+      doc.getElementById('newTodoTitle').value = '';
+  
+      
+    } else {
+      
+      alert('Please enter a valid todo.');
+    }
+  }
+  
