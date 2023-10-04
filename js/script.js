2a895c4a621af8f02425e6feb825b03bbe713a1e
Oleksii Tuhai <alextugai73@gmail.com>
2023-10-04 17:25:06 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ getData(api.todos)
     .then(data => {
         data.forEach(todo => todos.push(todo.body));
         renderTodos(todos, '.todos');
+        totalAmountOfTodos.innerHTML = `Total Todos: ${todos.length}`;
+
         console.log(todos);
     })
     .catch(error => {
@@ -23,17 +25,21 @@ getData(api.todos)
 
 
 // ----Code----
-let users = document.querySelector(".users");
-let history = document.querySelector(".history");
-let todosE = document.querySelectorAll(".todo");
-let totalAmountOfTodos = document.querySelector(".totalAmountOfTodos");
-let searchTodo = document.querySelector(".SearchTodo");
-let todoTitle = document.querySelectorAll(".todoTitle");
+const users = document.querySelector(".users");
+const history = document.querySelector(".history");
+const todosE = document.querySelectorAll(".todo");
+const todosList = document.querySelector(".todos");
+const totalAmountOfTodos = document.querySelector(".totalAmountOfTodos");
+const searchTodo = document.querySelector(".SearchTodo");
+const todoTitle = document.querySelectorAll(".todoTitle");
 
-setInterval(() => {
-    todosE = document.querySelectorAll(".todo");
-    totalAmountOfTodos.innerHTML = `Total Todos: ${todosE.length}`;
-}, 10);
+todosList.onchange = () => {
+    // todosE = document.querySelectorAll(".todo");
+    totalAmountOfTodos.innerHTML = `Total Todos: ${todos.length}`;
+};
+
+
+// ----functions-----
 
 function renderTodos(todos, parElSelector) {
     todos.forEach((todo, i) => {
@@ -47,13 +53,13 @@ function addTodo(perElSelector, title, id) {
     if (title.trim() !== '') {
 
         const newTodoItem = `
-        <li class="todo-item" id="${id}">
-        <div class="todo__content">
-          <div><span class="todo__number">${id}</span>.</div>
-          <input type="checkbox">
-          <div class="todo__title">${title}</div>
-        </div>
-        <button class="delete-button">DEL</button>
+        <li class="todo" id="todo${id}" style="margin: 0 0 2% 0;">
+          <div class="todoContent">
+            <div class="todoNumber">${id}.</div>
+            <input class="todoCheckbox${id}" type="checkbox">
+            <div class="todoTitle">${title}</div>
+          </div>
+          <button type="button" class="btn-delete" onclick="deleteE(0)" label="delete button">DEL</button>
         </li>
       `;
 
@@ -68,34 +74,34 @@ async function getData(url) {
     return data;
 }
 
-function selectUser(){
-    if(users.style.opacity == "1"){
+function selectUser() {
+    if (users.style.opacity == "1") {
         users.style.opacity = "0";
     }
-    else{
+    else {
         users.style.opacity = "1";
     }
 }
 
-function selectHistory(){
-    
-    if(history.style.opacity == "1"){
+function selectHistory() {
+
+    if (history.style.opacity == "1") {
         history.style.opacity = "0";
     }
-    else{
+    else {
         history.style.opacity = "1";
     }
 }
 
-function search(){
+function search() {
     let InnerHTMLArray = getInnerHTMLArray(todoTitle);
-    console.log(searchQueries(searchTodo.value,InnerHTMLArray))
+    console.log(searchQueries(searchTodo.value, InnerHTMLArray))
 }
 
-function filterFirst(){
+function filterFirst() {
 
 }
 
-function deleteE(id){
+function deleteE(id) {
     todosE[id].remove()
 }
