38da2e3c2b41406f1b1feeca5ae6f95a0b1bd525
Oleksii Tuhai <alextugai73@gmail.com>
2023-10-04 17:52:25 +0200

    add function that sends new todo to db.json (add addTodoButton.addEventListener('click') & addTodoToDbjson())

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const api = {
 let todos = [];
 
 
-
 getData(api.todos)
     .then(data => {
         data.forEach(todo => todos.push(todo.body));
@@ -33,6 +32,8 @@ const todosList = document.querySelector(".todos");
 const totalAmountOfTodos = document.querySelector(".totalAmountOfTodos");
 const searchTodo = document.querySelector(".SearchTodo");
 const todoTitle = document.querySelectorAll(".todoTitle");
+const addTodoButton = document.querySelector(".add");
+const inputTodo = document.querySelector(".createNewTodo");
 
 todosList.onchange = () => {
     // todosE = document.querySelectorAll(".todo");
@@ -42,35 +43,60 @@ todosList.onchange = () => {
 
 // ----functions-----
 
+addTodoButton.addEventListener("click", () => {
+    const newTodo = inputTodo.value.trim();
+
+    if (newTodo !== '') {
+        addTodoToDbjson({
+            "userId": 1,
+            "body": newTodo,
+            "completed": false
+        }).then(data => {
+            addTodo('.todos', newTodo, data.id);
+            inputTodo.value = '';
+        });
+    }
+});
+
 function renderTodos(todos, parElSelector) {
     todos.forEach((todo, i) => {
         addTodo(parElSelector, todo, i + 1);
     });
 }
 
-function addTodo(perElSelector, title, id) {
-    const parEl = doc.querySelector(perElSelector);
+function addTodo(parElSelector, title, id) {
+    const parEl = doc.querySelector(parElSelector);
 
     if (title.trim() !== '') {
-
         const newTodoItem = `
-        <li class="todo" id="todo${id}" style="margin: 0 0 2% 0;">
-          <div class="todoContent">
-            <div class="todoNumber">${id}.</div>
-            <input class="todoCheckbox${id}" type="checkbox">
-            <div class="todoTitle">${title}</div>
-          </div>
-          <button type="button" class="btn-delete" onclick="deleteE(0)" label="delete button">DEL</button>
-        </li>
-      `;
+            <li class="todo" id="todo${id}" style="margin: 0 0 2% 0;">
+              <div class="todoContent">
+                <div class="todoNumber">${id}.</div>
+                <input class="todoCheckbox${id}" type="checkbox">
+                <div class="todoTitle">${title}</div>
+              </div>
+              <button type="button" class="btn-delete" onclick="deleteE(${id})" label="delete button">DEL</button>
+            </li>
+          `;
 
         parEl.innerHTML += newTodoItem;
-
     }
 }
 
 async function getData(url) {
     const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+async function addTodoToDbjson(todoObj) {
+    const res = await fetch(api.todos, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todoObj)
+    });
     const data = await res.json();
     return data;
 }
